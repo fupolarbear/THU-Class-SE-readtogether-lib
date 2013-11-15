cde7bacbf1cf2fbda36f92e2641da008052c90b3
tuke <tuke1993@gmail.com>
2013-11-15 23:45:00 +0800

    pep8 initmodel

--- thesite/rt/initmodel.py
+++ thesite/rt/initmodel.py
@@ -5,10 +5,15 @@ NormalUser = Group.objects.create(name='NormalUser')
 AdvancedUser = Group.objects.create(name='AdvancedUser')
 Blacklist = Group.objects.create(name='Blacklist')
 Admin = Group.objects.create(name='Admin')
-comment = Permission.objects.create(codename='can_comment', \
-    name='can comment', content_type=dummy_type)
-manage = Permission.objects.create(codename='can_manage', name='can manage', content_type=dummy_type)
-search = Permission.objects.create(codename='can_search', name='can search', content_type=dummy_type)
+comment = Permission.objects.create(
+    codename='can_comment', name='can comment', content_type=dummy_type
+    )
+manage = Permission.objects.create(
+    codename='can_manage', name='can manage', content_type=dummy_type
+    )
+search = Permission.objects.create(
+    codename='can_search', name='can search', content_type=dummy_type
+    )
 NormalUser.permissions = [comment, search]
 AdvancedUser.permissions = [comment, search]
 Blacklist.permissions = [comment, search]
