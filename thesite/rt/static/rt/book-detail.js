809af73fe7d2b9a5ec9193d88f2b49278ff5c774
Fu Zuoyou <fupolarbear@gmail.com>
2013-11-16 16:02:26 +0800

    modify book-detail for ording, fix some template bug

---
+++ thesite/rt/static/rt/book-detail.js
@@ -0,0 +1,26 @@
+﻿
+$(document).ready(
+	function() {
+		$('#ordingModal').on('shown.bs.modal', function () {
+			console.log("modal");
+		});
+	}
+);
+
+//var hehe, m;
+function callording(obj, bid) {
+	//hehe = obj;
+	var list = $(obj).parent().parent().children().not($(obj).parent().parent().children().last());
+	
+	var content = $('#ordingModal').find('#ording-info');
+	content.html('');
+	$.each(list, function(i,val) {
+    	content.html(content.html() + $(val).html() + " ");
+    	//console.log(i + $(val).html());
+    	//hehe = val;
+	});
+	
+	$('#ordingModal').find("#book-id-info").html(bid);
+	
+	$('#ordingModal').modal('show');
+}
