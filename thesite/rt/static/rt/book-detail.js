6952ae7ea0c033ec8399840e289723dece38546d
Fu Zuoyou <fupolarbear@gmail.com>
2013-12-13 19:52:53 +0800

    safer booking url transmition, move out html-js to book-detail.js

--- thesite/rt/static/rt/book-detail.js
+++ thesite/rt/static/rt/book-detail.js
@@ -1,5 +1,6 @@
 ﻿
 //var hehe, m;
+//var hehe;
 function callording(obj, bid) {
 	//hehe = obj;
 	var list = $(obj).parent().parent().children().not($(obj).parent().parent().children().last());
@@ -13,7 +14,7 @@ function callording(obj, bid) {
 	});
 	
 	$('#ordingModal').find("#book-id-info").html(bid);
-	$('#ordingModal').find("#ording-book-id").val(bid);
+	$('#ordingModal').find("#ording-book-id").val($(obj).attr('myurl'));
 	
 	$('#ordingModal').modal('show');
 }
@@ -21,3 +22,58 @@ function callording(obj, bid) {
 function doording(bid){
 	console.log("bid:" + bid);
 }
+
+
+$(document).ready(
+	function() {
+
+		$('#comment-panel').jscroll({
+			debug: true,
+			autoTriggerUntil: 1,
+			//nextSelector: 'a.scroll-next:last'
+		});
+
+		csrftoken = getCookie('csrftoken');
+
+		$('#ordingModal').on('shown.bs.modal', function () {
+			console.log("finish display modal");
+		});
+
+		$('#ordingModal').find(".alert").hide();
+
+		$('#ording-button').click(function(){
+			var burl = $('#ordingModal').find("#ording-book-id").val();
+			console.log("get-burl:" + burl);
+
+			$.post(
+				burl,
+				{
+					csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val()
+				},
+				function(res){
+					var got = eval("(" + res + ")");
+					if(got.status == "OK"){
+						$('#ordingModal').find(".alert-success").show();
+						setTimeout(
+							function(){
+								$('#ordingModal').find(".alert").hide();
+								$('#ordingModal').modal('hide');
+							},
+							1200
+						);
+					} else {
+						$('#ordingModal').find(".alert-danger").find("#detailed-alert").html(got.err);
+						$('#ordingModal').find(".alert-danger").show();
+						setTimeout(
+							function(){
+								$('#ordingModal').find(".alert").hide();
+								$('#ordingModal').modal('hide');
+							},
+							2000
+						);
+					}
+				}
+			);
+		});
+	}
+);
