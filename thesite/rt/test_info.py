f54ec5fd4bab9c5639af6bfdf8a90547cba246c3
ZhangSihao <zhangsh0711@gmail.com>
2013-12-14 14:36:02 +0800

    a bunch of test

--- thesite/rt/test_info.py
+++ thesite/rt/test_info.py
@@ -18,21 +18,21 @@ class MySeleniumTests(LiveServerTestCase):
 
     def test_info(self):
         self.selenium.get(self.live_server_url)
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[3]/div/div[2]/ul/li[3]/a').click()  # Info
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[3]/div/div[2]/ul/li/a').click()  # Home
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[5]/div/div[2]/div/table/tbody/tr/td[2]/a'
-        ).click()  # Mark
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[5]/div/div[2]/div/div/div/div/div/div/h4/a'
-        ).click()  # Mark Nov.14
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[5]/div/div[2]/div/ul/li[2]/a').click()  # UserGuide
-    time.sleep(0.2)
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[5]/div/div[2]/div/div/div[2]/div/div/div/h4/a'
-        ).click()  # guide!
-    self.selenium.find_element_by_xpath(
-        '/html/body/div[3]/div/div/a').click()  # ReadTogether
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[3]/div/div[2]/ul/li[3]/a').click()  # Info
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[3]/div/div[2]/ul/li/a').click()  # Home
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[5]/div/div[2]/div/table/tbody/tr/td[2]/a'
+            ).click()  # Mark
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[5]/div/div[2]/div/div/div/div/div/div/h4/a'
+            ).click()  # Mark Nov.14
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[5]/div/div[2]/div/ul/li[2]/a').click()  # UserGuide
+        time.sleep(0.2)
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[5]/div/div[2]/div/div/div[2]/div/div/div/h4/a'
+            ).click()  # guide!
+        self.selenium.find_element_by_xpath(
+            '/html/body/div[3]/div/div/a').click()  # ReadTogether
