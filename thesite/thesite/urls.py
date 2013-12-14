cdb5bcc793bf3db8d55fdbeb57cc9897eeb1c61e
武祥晋 <taccoraw@gmail.com>
2013-12-14 15:43:01 +0800

    remove admindoc. django /admin/ -> /root/.

--- thesite/thesite/urls.py
+++ thesite/thesite/urls.py
@@ -8,8 +8,8 @@ urlpatterns = patterns('',
     url(r'^', include('rt.urls', namespace='rt')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^root/', include(admin.site.urls)),
 )
